4920997c9d081b0d7e2f5ff3f5ce137d9a20796f
LamBregaani <55120859+LamBregaani@users.noreply.github.com>
2024-05-23 21:22:04 -0400

    Initial commit

---
+++ Asteroids.cpp
@@ -0,0 +1,13 @@
+#include "Scripts/Headers/Entity.h"
+using Asteroids::Entity;
+
+#include "Scripts/Headers/EntityManager.h"
+using Asteroids::EntityManager;
+
+#include <iostream>
+using std::cout;
+
+int main()
+{
+   return 0;
+}
