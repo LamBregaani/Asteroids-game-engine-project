47bed775c462a49f27c511783f5ef798ef6a8178
LamBregaani <55120859+LamBregaani@users.noreply.github.com>
2024-05-24 10:30:09 -0400

    Added Vector2 struct
    Vector2 can be used to represent coordinates in 2d space
    Includes operators for adding, subtracting, dividing, multiplying, and comparing vector2s
    Includes other general functions for using 2d vectors

---
+++ Scripts/Headers/Vector2.h
@@ -0,0 +1,84 @@
+#pragma once
+namespace Asteroids
+{
+	struct Vector2
+	{
+	public:
+		float x = 0;
+		float y = 0;
+
+		Vector2() {}
+
+		Vector2(float xin, float yin)
+			:x(xin),
+			y(yin)
+		{
+
+		}
+
+		Vector2 operator + (const Vector2& rhs) const
+		{
+			return Vector2(x + rhs.x, y + rhs.y);
+		}
+
+
+		Vector2 operator - (const Vector2& rhs) const
+		{
+			return Vector2(x - rhs.x, y - rhs.y);
+		}
+
+		Vector2 operator * (float rhs) const
+		{
+			return Vector2(x * rhs, y * rhs);
+		}
+
+		Vector2 operator / (float rhs) const
+		{
+			return Vector2(x / rhs, y / rhs);
+		}
+
+		bool operator == (const Vector2& rhs)
+		{
+			if (x == rhs.x && y == rhs.y)
+				return true;
+
+			return false;
+
+		}
+
+		bool operator != (const Vector2& rhs)
+		{
+			if (x != rhs.x || y != rhs.y)
+				return true;
+
+			return false;
+
+		}
+
+		void operator += (const Vector2& rhs)
+		{
+			x += rhs.x;
+			y += rhs.y;
+		}
+
+		Vector2 Zero()
+		{
+			return Vector2(0, 0);
+		}
+
+		Vector2& Scale(float s)
+		{
+			x *= s;
+			y *= s;
+			return *this;
+		}
+
+		static float Distance(Vector2& a, Vector2& b);
+
+		static Vector2& Direction(Vector2& origin, Vector2& target);
+
+		static Vector2& Normalized(Vector2& vec);
+
+		Vector2& normalized();
+	};
+}
---
+++ Scripts/Vector2.cpp
@@ -0,0 +1,49 @@
+#include "Headers/Vector2.h"
+using Asteroids::Vector2;
+#include <cmath>
+
+float Vector2::Distance(Vector2& a, Vector2& b)
+{
+	float d;
+
+	float diffX = a.x - b.x;
+
+	float diffY = a.y - b.y;
+
+	d = sqrt((diffY * diffY) + (diffX * diffX));
+
+	return d;
+}
+
+Vector2& Vector2::Direction(Vector2& origin, Vector2& target)
+{
+	Vector2 dir;
+
+	dir = target - origin;
+
+	return dir;
+}
+
+Vector2& Vector2::Normalized(Vector2& vec)
+{
+	Vector2 norm;
+
+	float w = sqrt(vec.x * vec.x + vec.y * vec.y);
+	norm.x  = vec.x / w;
+	norm.y	= vec.y / w;
+
+
+	return norm;
+}
+
+Vector2& Vector2::normalized()
+{
+	Vector2 norm;
+
+	float w = sqrt(x * x + y * y);
+	norm.x = x / w;
+	norm.y = y / w;
+
+
+	return norm;
+}
